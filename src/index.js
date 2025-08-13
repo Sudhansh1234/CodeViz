1e90c9e4e06b5c8b155f36234837691b7cd39959
Sudhansh1234 <100071590+Sudhansh1234@users.noreply.github.com>
2025-08-13 14:41:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import "./index.css";
 
 // Point Monaco to local /public path via environment + inline worker bootstrap
 // This prevents "toUrl" undefined and CSP issues.
+// eslint-disable-next-line no-restricted-globals
 self.MonacoEnvironment = {
   baseUrl: "/monaco-editor/min/vs",
   getWorkerUrl: function (moduleId, label) {
     // Create a simple worker that can handle basic editor operations
     const workerCode = `
       // Basic Monaco Editor worker
+      // eslint-disable-next-line no-restricted-globals
       self.onmessage = function(e) {
         // Handle basic editor operations
         if (e.data && e.data.type === 'init') {
+          // eslint-disable-next-line no-restricted-globals
           self.postMessage({ type: 'ready' });
         }
       };
