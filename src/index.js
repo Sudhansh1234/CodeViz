78cbf99395a59960fdabbf51588306797c6aeed1
Sudhansh1234 <100071590+Sudhansh1234@users.noreply.github.com>
2025-08-13 14:46:25 +0530

    ESLint errors fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -5,12 +6,10 @@ import "./index.css";
 
 // Point Monaco to local /public path via environment + inline worker bootstrap
 // This prevents "toUrl" undefined and CSP issues.
-// eslint-disable-next-line no-restricted-globals
 self.MonacoEnvironment = {
   baseUrl: "/monaco-editor/min/vs",
   getWorkerUrl: function (moduleId, label) {
     // Create a simple worker that can handle basic editor operations
-    /* eslint-disable no-restricted-globals */
     const workerCode = `
       // Basic Monaco Editor worker
       self.onmessage = function(e) {
@@ -20,7 +19,6 @@ self.MonacoEnvironment = {
         }
       };
     `;
-    /* eslint-enable no-restricted-globals */
     
     return URL.createObjectURL(
       new Blob([workerCode], { type: "application/javascript" })
